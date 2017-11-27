6909b9ba49c970728b303c2c21a40fa64c470542
qazsato <qazsato@gmail.com>
2017-11-27 20:04:40 +0900

    DynamoDBへの登録処理追加

--- handler.js
+++ handler.js
@@ -1,16 +1,11 @@
 'use strict';
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB.DocumentClient();
 
 module.exports.long2short = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  const longUrl = event.queryStringParameters.longUrl;
+  dynamo.put({TableName: 'shorturl', Item: {
+    id: '1',
+    long_url: longUrl
+  }}, callback);
 };
